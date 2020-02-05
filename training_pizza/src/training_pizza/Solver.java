6a615a65ff95b314b9bed303a35dfb10c402b94b
jfont <jfont@usj.es>
2020-02-05 18:05:11 +0100

    bug

--- training_pizza/src/training_pizza/Solver.java
+++ training_pizza/src/training_pizza/Solver.java
@@ -1,40 +1,51 @@
 package training_pizza;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 
-public class Solver {
+public class Solver extends Thread {
 	
 	String text;
-	public Solver(String path) {
-		
-		for(int i=0;i<100;i++) {
+	String name;
+	String path;
+	long startTime;
+	int times;
+	
+	public Solver(String path,String name,int times) {
+		this.name = name;
+		this.path = path;
+		this.times = times;
+		this.startTime = System.currentTimeMillis();
+	}
+	
+	
+	@Override
+	public synchronized void start() {
+		// TODO Auto-generated method stub
+		super.start();
+		System.out.println("Empiezo!: "+this.startTime);
+		for(int i=0;i<this.times;i++) {
 			readFile(path);
 			writeToFile(i+".txt");
 		}
-		
+		long timeSpent = System.currentTimeMillis() - this.startTime;
+		System.out.println(this.name+" - "+timeSpent+" - "+timeSpent/(float)this.times);
 	}
-	
 	private void writeToFile(String string) {
 		
 		try {
-			PrintStream fw = new PrintStream("out/"+string);
+			PrintStream fw = new PrintStream(new File("out/"+name+"/"+string));
 			
 			fw.print(this.text);
 			
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
-		
-		
+		}	
 	}
 
 	private void readFile(String path) {
@@ -44,8 +55,7 @@ public class Solver {
 			String line = br.readLine();
 			
 			while((line = br.readLine()) != null) {
-				text += line;
-				
+				text += line;	
 			}
 			
 		} catch (IOException e) {

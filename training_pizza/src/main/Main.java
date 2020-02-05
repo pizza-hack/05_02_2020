c2bafc2c8e2c71d486ad2395e1308267ea1f473e
jfont <jfont@usj.es>
2020-02-05 18:45:55 +0100

    updated structure

---
+++ training_pizza/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hllo world");
+		//long t0 = System.currentTimeMillis();
+		Solver s1 = new Solver("in/a_example.txt","1",100);
+		//s1.start();
+		Solver s2 = new Solver("in/b_lovely_landscapes.txt","2",1);
+		//s2.start();
+		Solver s3 = new Solver("in/c_memorable_moments.txt","3",10);
+		s3.start();
+
+		//System.out.println(System.currentTimeMillis() - t0);
+	}
+}
---
+++ training_pizza/src/main/Solver.java
@@ -0,0 +1,70 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintStream;
+
+public class Solver extends Thread {
+	
+	String text;
+	String name;
+	String path;
+	long startTime;
+	int times;
+	
+	public Solver(String path,String name,int times) {
+		this.name = name;
+		this.path = path;
+		this.times = times;
+		this.startTime = System.currentTimeMillis();
+	}
+	
+	
+	@Override
+	public synchronized void start() {
+		// TODO Auto-generated method stub
+		super.start();
+		System.out.println("Empiezo!: "+this.startTime);
+		for(int i=0;i<this.times;i++) {
+			readFile(path);
+			writeToFile(i+".txt");
+		}
+		long timeSpent = System.currentTimeMillis() - this.startTime;
+		System.out.println(this.name+" - "+timeSpent+" - "+timeSpent/(float)this.times);
+	}
+	private void writeToFile(String string) {
+		
+		try {
+			PrintStream fw = new PrintStream(new File("out/"+name+"/"+string));
+			
+			fw.print(this.text);
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+	}
+
+	private void readFile(String path) {
+		try {
+			
+			BufferedReader br = new BufferedReader(new FileReader(path));
+			String line = br.readLine();
+			
+			while((line = br.readLine()) != null) {
+				//text += line;	
+				System.out.println(line);
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+}
